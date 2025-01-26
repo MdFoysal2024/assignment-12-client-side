59f8444e039dfaae4b241575976a171d772f098f
MdFoysal2024 <mdfoysal010199@gmail.com>
2025-01-26 19:26:31 +0600

    Stripe Payment Gateway Added to the Project

--- src/pages/FundingPage/CheckoutForm.jsx
+++ src/pages/FundingPage/CheckoutForm.jsx
@@ -1,7 +1,25 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 
 const CheckoutForm = ({ donorName, donationDate, donationAmount }) => {
+    const axiosSecure = useAxiosSecure()
+
+    const [error, setError] = useState();
+    const { user } = useAuth();
+    const stripe = useStripe();
+    const navigate = useNavigate();
+    const elements = useElements();
+    const [transactionId, setTransactionId] = useState();
+    const [clientSecret, setClientSecret] = useState('')
+
+
+
+
 
 
     const donorInfo = {
@@ -11,17 +29,147 @@ const CheckoutForm = ({ donorName, donationDate, donationAmount }) => {
     }
     console.log(donorInfo);
 
+
+
+    useEffect(() => {
+        if (donationAmount > 0) {
+            axiosSecure.post('/create-payment-intent', { price: donationAmount }) 
+            //price কে  সার্ভার এর app.post('/create-payment-intent' থেকে আনা হলো ,
+                .then(res => {
+                    console.log(res.data.clientSecret);
+
+                    //taken from server---> সার্ভার থেকে clientSecret কে এনে  console.log(res.data.clientSecret); এবং setClientSecret() এর ভিতরে সেট করা হয়ছে।  এবং ঐ সার্ভারের clientSecret এর মান  const [clientSecret, setClientSecret] = useState('') এর clientSecret এর ভিতরে মান হিসাবে সেট হবে। যদিও useState('')  এর clientSecret এর নাম এবং সার্ভারের clientSecret এর নাম একই , কিন্তু সার্ভার থেকে clientSecret এনে  setClientSecret() এর ভিতরে সেট করলে useState('')  এর clientSecret ভিতরে মান সেট হবে।
+
+                    setClientSecret(res.data.clientSecret)
+
+                })
+        }
+    }, [donationAmount])
+
+
+
+
+
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            return ;
+            // যদি stripe/elements না পায় তাহলে return  করে দিবে।
+            // Stripe.js has not loaded yet. Make sure to disable
+            // form submission until Stripe.js has loaded.
+        }
+
+
+        const card = elements.getElement(CardElement);
+        if (card === null) {
+            return;
+            // if (!card) or if (card === null)-->যদি card না পায় তাহলেও return  করে দিবে।
+        }
+
+        
+        // const donorInfo = {
+        //     donorName,
+        //     donationDate,
+        //     donationAmount
+        // }
+        console.log(donorInfo);
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        if (error) {
+            console.log('payment error', error)
+            setError(error.message);
+        } if (paymentMethod) {
+            console.log('payment method', paymentMethod)
+            setError(' ');
+        }
+
+
+        //taken from ---->Custom payment flow-->
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card, billing_details: {
+                    email: user?.email || 'anonymous',
+                    name: user?.displayName || 'anonymous'
+                }
+            }
+        })
+
+        if (confirmError) {
+            console.log('confirm error')
+        }
+        else {
+            console.log('payment intent', paymentIntent);
+            if (paymentIntent.status === 'succeeded') {
+                //inspect > console > paymentIntent > id and status : succeeded  -->
+                console.log('transaction id', paymentIntent.id);
+                setTransactionId(paymentIntent.id)
+            }
+        }
+
+        // // save the payment info in to the database ---->
+        // const payment = {
+        //     email: user.email,
+        //     price: totalPrice,
+        //     transactionId: paymentIntent.id,
+        //     data: new Date(), // have to convert in utc time (use moment js)
+        //     cartIds: cart.map(item => item._id), // _id is present db id--> order লিষ্ট হতে my cart এ add করার id
+        //     menuItemIds: cart.map(item => item.menuId), // order লিষ্ট/ফুড লিস্টে নতুন item add করার সময় db তে যে _id তৈরী হয়েছিলো ঐ _id দিয়ে item কে যখন my cart এ add করা হয় তখন আগের _id কে menuId হিসাবে db তে post করা হয়, এবং নতুন করে অঅরো একটি _id তৈরী হয় ।
+        //     status: 'pending'
+        // }
+
+        // const res = await axiosSecure.post('/payments', payment)
+        // console.log('Payment Saved', res.data);
+        // refetch();
+        // if (res.data?.paymentResult?.insertedId) {
+        //     Swal.fire({
+        //         position: "center",
+        //         icon: "success",
+        //         title: "Thanks For Payment Successfully",
+        //         showConfirmButton: false,
+        //         timer: 1500
+        //     });
+        //     navigate('/dashboard/paymentHistory');
+        // }
+    }
+
+
+
     return (
-        <div>
+        <form onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <button className="bg-red-600 text-lg my-4 text-white font-semibold px-12 py-1 " type="submit" disabled={!stripe || !clientSecret} >
+                Donate
+            </button>
 
+            
+            <p className="text-red-500 font-bold">{error}</p>
+            {
+                transactionId && <p>Your Transaction ID: {transactionId}  </p>
+            }
 
-            <p>{donorName}, </p>
-            <p>{format(new Date(donationDate), 'P')},</p>
-            <p>${donationAmount}</p>
-
-
-
-        </div>
+        </form>
     );
 };
 
--- src/pages/FundingPage/Donation.jsx
+++ src/pages/FundingPage/Donation.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import CheckoutForm from './CheckoutForm';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Donation = () => {
     const location = useLocation();
@@ -13,13 +16,24 @@ const Donation = () => {
 
     return (
         <div className='container mx-auto py-24'>
-            Donate by stripe method
-           
-            <CheckoutForm
-                donorName={donorName}
-                donationDate={donationDate}
-                donationAmount={donationAmount}
-            ></CheckoutForm>
+          
+
+
+            {/* -------Donation Section start------- */}
+
+            <div className='p-24 mt-12 bg-red-100'>
+                <Elements stripe={stripePromise}>
+
+                    <CheckoutForm
+                        donorName={donorName}
+                        donationDate={donationDate}
+                        donationAmount={donationAmount}
+                    ></CheckoutForm>
+
+                </Elements>
+            </div>
+
+
 
         </div>
     );
