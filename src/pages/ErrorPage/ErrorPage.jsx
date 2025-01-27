727cbb8c3d91cdec7441c97060c4859f4981d29a
MdFoysal2024 <mdfoysal010199@gmail.com>
2025-01-27 10:00:59 +0600

    Error Page IS WORKING SUCCESSFULLY

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import errorImg from '../../images/error_page.png'
+
+import { Link } from 'react-router-dom';
 const ErrorPage = () => {
     return (
-        <div>
+        <div className='flex justify-center max-w-lg md:max-w-2xl mx-auto bg-red-600 items-center mt-48'>
 
             
             <div className="hero-content text-neutral-content text-center">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                <div className=" py-8">
+                    <h1 className="mb-5 text-6xl md:text-9xl font-bold">404</h1>
+                    <p className="mb-5 font-bold">
+                        WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND.
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <Link to='/'>
+                    <button className="btn bg-white px-8 text-red-600 hover:text-black text-xl font-bold">Go Home</button>
+                    </Link>
                 </div>
             </div>
 
